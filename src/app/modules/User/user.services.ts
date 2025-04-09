2c58c9c6e713a96e9307b83c2e1bfd1629e29033
Md Moniruzzaman <web.moniruzzaman1@gmail.com>
2025-04-09 16:23:14 +0600

    create admin services done

--- src/app/modules/User/user.services.ts
+++ src/app/modules/User/user.services.ts
@@ -1,9 +1,26 @@
 
+import { PrismaClient, UserRole } from "@prisma/client"
 
-const createAdmin = async () => {
-    return {
-        message: 'admin created'
+const prisma = new PrismaClient()
+
+const createAdmin = async (data: any) => {
+    const userData = {
+        email: data.admin.email,
+        password: data.password,
+        role: UserRole.ADMIN
     }
+
+    const result = await prisma.$transaction(async (transactionClient) => {
+         await transactionClient.user.create({
+            data: userData
+        })
+        const createAdminData = await transactionClient.admin.create({
+            data: data.admin
+        })
+        return createAdminData
+    })
+
+    return result
 }
 
 export const userServices = {
