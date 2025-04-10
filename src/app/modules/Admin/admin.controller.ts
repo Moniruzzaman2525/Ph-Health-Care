5c5d2128a44a814f8ae9371e6df8c1390de4a9cf
Md Moniruzzaman <web.moniruzzaman1@gmail.com>
2025-04-10 19:08:12 +0600

    done pick

--- src/app/modules/Admin/admin.controller.ts
+++ src/app/modules/Admin/admin.controller.ts
@@ -2,10 +2,21 @@ import { Request, Response } from "express";
 import { adminServices } from "./admin.services";
 
 
+const pick = <T extends Record<string, unknown>, K extends keyof T>(obj: T, keys: K[]): Partial<T> => {
+    const finalObj: Partial<T> = {};
+    for (const key of keys) {
+        if (obj && Object.hasOwnProperty.call(obj, key)) {
+            finalObj[key] = obj[key]
+        }
+    }
+    return finalObj
+}
+
 const getAllAdminFromDb = async (req: Request, res: Response) => {
 
     try {
-        const result = await adminServices.getAllAdminFromDb(req.query)
+        const filter = pick(req.query, ['name', 'email', 'searchTerm'])
+        const result = await adminServices.getAllAdminFromDb(filter)
 
         res.status(200).json({
             success: true,
