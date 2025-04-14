512387776d89cf1d8ea8e41d84449df15a66407a
Md Moniruzzaman <web.moniruzzaman1@gmail.com>
2025-04-14 19:05:23 +0600

    router done

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,14 @@
 import express, { Application, Request, Response } from 'express';
 import cors from 'cors'
-import { userRoutes } from './app/modules/User/user.routes';
-import { AdminRoutes } from './app/modules/Admin/admin.routes';
+import router from './app/routes';
+
 
 const app: Application = express();
 
 app.use(express.json());
 app.use(cors())
 
-app.use('/api/v1/user', userRoutes)
-app.use('/api/v1/admin', AdminRoutes)
+app.use('/api/v1', router)
 
 
 app.get('/', (req: Request, res: Response) => {
