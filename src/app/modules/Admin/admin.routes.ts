27d574bc002dc3ae168db7e19e7891f7bc86ff37
Md Moniruzzaman <web.moniruzzaman1@gmail.com>
2025-04-15 09:15:11 +0600

    validate request

--- src/app/modules/Admin/admin.routes.ts
+++ src/app/modules/Admin/admin.routes.ts
@@ -1,13 +1,31 @@
 
-import express from 'express'
+import express, { NextFunction, Request, Response } from 'express'
 import { adminController } from './admin.controller'
+import { AnyZodObject, z } from 'zod'
 
 
 const router = express.Router()
 
+const update = z.object({
+    body: z.object({
+        name: z.string(),
+        contactNumber: z.string()
+    })
+})
+
+const validateRequest = (schema: AnyZodObject) => async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        await schema.parseAsync(req.body)
+        next()
+    } catch (error) {
+        next(error)
+    }
+}
+
+
 router.get('/', adminController.getAllAdminFromDb)
 router.get('/:id', adminController.getByIdFromDb)
-router.patch('/:id', adminController.updateFromDb)
+router.patch('/:id', validateRequest(update), adminController.updateFromDb)
 router.delete('/:id', adminController.deleteFromDb)
 router.delete('/soft/:id', adminController.softDeleteFromDb)
 
