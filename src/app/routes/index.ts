4df5f927c53070c11d5f07dbb4998760b20a6eb8
Md Moniruzzaman <web.moniruzzaman1@gmail.com>
2025-04-15 17:01:31 +0600

    link to route

--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -1,5 +1,5 @@
 import express from 'express'
-import { userRoutes } from '../modules/User/user.routes'
+import { UserRoutes } from '../modules/User/user.routes'
 import { AdminRoutes } from '../modules/Admin/admin.routes'
 import { AuthRoutes } from '../modules/Auth/auth.routes'
 
@@ -8,7 +8,7 @@ const router = express.Router()
 const modulesRoute = [
     {
         path: '/user',
-        route: userRoutes
+        route: UserRoutes
     },
     {
         path: '/admin',
