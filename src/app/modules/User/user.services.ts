6aa9299665448046ab10597075936b9d4f5b0988
Md Moniruzzaman <web.moniruzzaman1@gmail.com>
2025-04-25 08:39:06 +0600

    working on update profile

--- src/app/modules/User/user.services.ts
+++ src/app/modules/User/user.services.ts
@@ -1,5 +1,5 @@
 
-import { Admin, Doctor, Patient, Prisma, UserRole } from "@prisma/client"
+import { Admin, Doctor, Patient, Prisma, UserRole, UserStatus } from "@prisma/client"
 import * as bcrypt from "bcrypt"
 import prisma from "../../../helpers/prisma"
 import { fileUploader } from "../../../helpers/fileUploader"
@@ -174,7 +174,8 @@ const getMyProfile = async (user: any) => {
 
     const userInfo = await prisma.user.findUnique({
         where: {
-            email: user.email
+            email: user.email,
+            status: UserStatus.ACTIVE
         },
         select: {
             id: true,
@@ -225,6 +226,48 @@ const getMyProfile = async (user: any) => {
 
 
 const updateMyProfile = async (user: any, payload: any) => {
+
+    const userInfo = await prisma.user.findUniqueOrThrow({
+        where: {
+            email: payload.email,
+            status: UserStatus.ACTIVE
+        }
+    })
+    let profileInfo
+
+    if (userInfo?.role === UserRole.SUPER_ADMIN) {
+        profileInfo = await prisma.admin.update({
+            where: {
+                email: userInfo.email
+            },
+            data: payload
+        })
+    }
+    else if (userInfo?.role === UserRole.ADMIN) {
+        profileInfo = await prisma.admin.update({
+            where: {
+                email: userInfo.email
+            },
+            data: payload
+        })
+    }
+    else if (userInfo?.role === UserRole.DOCTOR) {
+        profileInfo = await prisma.doctor.update({
+            where: {
+                email: userInfo.email
+            },
+            data: payload
+        })
+    }
+    else if (userInfo?.role === UserRole.PATIENT) {
+        profileInfo = await prisma.patient.update({
+            where: {
+                email: userInfo.email
+            },
+            data: payload
+        })
+    }
+
 
 }
 
