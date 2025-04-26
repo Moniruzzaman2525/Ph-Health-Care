6c08f6eabc0eaa670f1a2212ab6e31abbf68c85f
Md Moniruzzaman <web.moniruzzaman1@gmail.com>
2025-04-26 09:04:49 +0600

    start doctor speciliets

--- src/app/modules/Doctor/doctor.services.ts
+++ src/app/modules/Doctor/doctor.services.ts
@@ -2,20 +2,40 @@ import prisma from "../../../helpers/prisma"
 
 
 const updateIntoDb = async (id: string, payload: any) => {
+
+    const { specialties, ...doctorData } = payload
+
     const doctor = await prisma.doctor.findFirstOrThrow({
         where: {
             id
-       }
+        }
     })
 
-    const updateDoctorData = await prisma.doctor.update({
-        where: {
-            id
-        },
-        data: payload
+    const result = await prisma.$transaction(async (transactionClient) => {
+        const updateDoctorData = await transactionClient.doctor.update({
+            where: {
+                id
+            },
+            data: doctorData,
+            include: {
+                doctorSpecialties: true
+            }
+        })
+
+        for (const specialtiesId of specialties) {
+            const createDoctorSpecialties = await transactionClient.doctorSpecialties.create({
+                data: {
+                    doctorId: doctor.id,
+                    specialtiesId: specialtiesId
+                }
+            })
+        }
+
+        return updateDoctorData
     })
 
-    return updateDoctorData
+
+    return result
 }
 
 
