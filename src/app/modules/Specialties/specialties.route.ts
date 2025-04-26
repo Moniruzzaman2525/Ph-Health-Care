641cfaac0deb5fac143ac7619cff7f72f995be2d
Md Moniruzzaman <web.moniruzzaman1@gmail.com>
2025-04-26 08:30:23 +0600

    done create

--- src/app/modules/Specialties/specialties.route.ts
+++ src/app/modules/Specialties/specialties.route.ts
@@ -2,13 +2,14 @@
 import express, { NextFunction, Request, Response } from 'express'
 import { SpecialtiesController } from './specialties.controller'
 import { fileUploader } from '../../../helpers/fileUploader'
+import { specialtiesValidationSchemas } from './specialties.validation'
 
 const router = express.Router()
 
 router.post("/", fileUploader.upload.single('file'),
     (req: Request, res: Response, next: NextFunction) => {
 
-        req.body = JSON.parse(req.body.data)
+        req.body = specialtiesValidationSchemas.create.parse(JSON.parse(req.body.data))
         return SpecialtiesController.insertIntoDB(req, res, next)
     }
 )
--- src/app/modules/Specialties/specialties.services.ts
+++ src/app/modules/Specialties/specialties.services.ts
@@ -1,11 +1,12 @@
 import { Request } from "express"
 import { fileUploader } from "../../../helpers/fileUploader"
 import prisma from "../../../helpers/prisma"
+import { IFile } from "../../interfaces/file"
 
 
 const insertIntoDB = async (req: Request) => {
 
-    const file = req.file
+    const file = req.file as IFile | undefined
 
     if (file) {
         const uploadToCloudinary = await fileUploader.uploadToCloudinary(file)
