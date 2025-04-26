823081d9d439d48bf939dff8152e4c58278715e7
Md Moniruzzaman <web.moniruzzaman1@gmail.com>
2025-04-26 08:27:13 +0600

    update

--- src/app/modules/Specialties/specialties.controller.ts
+++ src/app/modules/Specialties/specialties.controller.ts
@@ -8,7 +8,7 @@ import { SpecialtiesServices } from "./specialties.services";
 
 
 const insertIntoDB = catchAsync(async (req: Request, res: Response, next: NextFunction) => {
-    const result = await SpecialtiesServices.insertIntoDB()
+    const result = await SpecialtiesServices.insertIntoDB(req)
 
     sendResponse(res, {
         statusCode: httpStatus.OK,
--- src/app/modules/Specialties/specialties.services.ts
+++ src/app/modules/Specialties/specialties.services.ts
@@ -1,7 +1,22 @@
+import { Request } from "express"
+import { fileUploader } from "../../../helpers/fileUploader"
+import prisma from "../../../helpers/prisma"
 
 
-const insertIntoDB = async () => {
-    
+const insertIntoDB = async (req: Request) => {
+
+    const file = req.file
+
+    if (file) {
+        const uploadToCloudinary = await fileUploader.uploadToCloudinary(file)
+        req.body.icon = uploadToCloudinary?.secure_url
+    }
+
+    const result = await prisma.specialties.create({
+        data: req.body
+    })
+
+    return result
 }
 
 
