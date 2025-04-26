dd34d4b05ae6523729454bafa8468059a40fc32b
Md Moniruzzaman <web.moniruzzaman1@gmail.com>
2025-04-26 08:48:30 +0600

    update doctor

--- src/app/modules/Doctor/doctor.controller.ts
+++ src/app/modules/Doctor/doctor.controller.ts
@@ -2,12 +2,13 @@ import { assert } from "console";
 import catchAsync from "../../../shared/catchAsync";
 import sendResponse from "../../../shared/sendResponse";
 import httpStatus from "http-status";
+import { DoctorServices } from "./doctor.services";
 
 
 
 const updateIntoDb = catchAsync(async (req, res, next) => {
     const id = req.params.id
-    const result = await DcotorServices.updateIntoDb(id, req.body)
+    const result = await DoctorServices.updateIntoDb(id, req.body)
 
     sendResponse(res, {
         success: true,
