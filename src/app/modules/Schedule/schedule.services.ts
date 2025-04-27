12b3a6b26755dcaeb4b01d0e1fdcb36a470edc79
Md Moniruzzaman <web.moniruzzaman1@gmail.com>
2025-04-27 12:05:30 +0600

    working on schedule services

--- src/app/modules/Schedule/schedule.services.ts
+++ src/app/modules/Schedule/schedule.services.ts
@@ -1,9 +1,17 @@
+import { addHours } from "date-fns"
 
 
 const insertIntoDb = async (payload: any) => {
 
-    const {startData, endData, startTime, endTime} = payload
+    const { startDate, endDate, startTime, endTime } = payload
 
+    const currentData = new Date(startDate)
+    const lastData = new Date(endDate)
+    while (currentData <= lastData) {
+        const startDateTime = new Date(
+             addHours()
+         )
+    }
 }
 
 export const scheduleServices = {
