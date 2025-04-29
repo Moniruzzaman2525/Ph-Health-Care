555830eac1df0e1aac42f755316eb10a20bcf927
Md Moniruzzaman <web.moniruzzaman1@gmail.com>
2025-04-29 08:08:34 +0600

    appointment schedule done

--- src/app/modules/Appointment/appointment.services.ts
+++ src/app/modules/Appointment/appointment.services.ts
@@ -1,16 +1,40 @@
 import prisma from "../../../helpers/prisma"
 import { IAuthUser } from "../../interfaces/common"
+import { v4 as uuidv4 } from 'uuid';
 
 
 const createAppointment = async (user: IAuthUser, payload: any) => {
 
-    console.log(payload)
     const patientData = await prisma.patient.findFirstOrThrow({
         where: {
             email: user?.email
         }
     })
-    console.log(patientData)
+
+    const doctorData = await prisma.doctor.findUniqueOrThrow({
+        where: {
+            id: payload.doctorId
+        }
+    })
+    await prisma.doctorSchedules.findFirstOrThrow({
+        where: {
+            doctorId: payload.doctorId,
+            scheduleId: payload.scheduleId,
+            isBooked: false
+        }
+    })
+
+    const videoCallingId = uuidv4()
+
+    const result = await prisma.appointment.create({
+        data: {
+            patientId: patientData.id,
+            doctorId: doctorData.id,
+            scheduleId: payload.scheduleId,
+            videoCallingId: videoCallingId
+        }
+    })
+    return result
 }
 
 export const AppointmentServices = {
